a8b662b8542bd0a96794db5f615d3ebd2a79a43b
danielVg <danielveragi@gmail.com>
2018-07-21 08:05:33 +0200

    douböle quotes
    Signed-off-by: danielVg <danielveragi@gmail.com>

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -1,39 +1,39 @@
 
-const express = require('express');
+const express = require("express");
 // eslint-disable-next-line no-unused-vars
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const router = express.Router();
-const debug = require('debug')('APP:routes');
+const debug = require("debug")("APP:routes");
 
 // Schema
-const model = require('../../models/model');
+const model = require("../../models/model");
 
 // Basic route Handler
-router.get('/', (req, res) => {
-  debug('Rendering index.hbs');
-  res.status(200).render('home');
+router.get("/", (req, res) => {
+	debug("Rendering index.hbs");
+	res.status(200).render("home");
 });
 
 /* CRUD Operations */
 
 // Create
-router.post('path', (req, res) => {
+router.post("path", (req, res) => {
 
 });
 
 // Read
-router.get('path', (req, res) => {
+router.get("path", (req, res) => {
 
 });
 
 // Update
-router.put('path', (req, res) => {
+router.put("path", (req, res) => {
 
 });
 
 // Delete
-router.delete('path', (req, res) => {
+router.delete("path", (req, res) => {
 
 });
 
