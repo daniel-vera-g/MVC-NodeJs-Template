47b2791a044ea59813df0578622b227a4c3a2ada
danielVg <danielveragi@gmail.com>
2018-07-21 07:54:25 +0200

    changes eslintrc
    Signed-off-by: danielVg <danielveragi@gmail.com>

---
+++ config/winston.js
@@ -0,0 +1,11 @@
+/**
+ * @author Daniel V.G.
+ * @email danielveragi@gmail.com
+ * @create date 2018-07-21 07:51:16
+ * @modify date 2018-07-21 07:51:16
+ * @desc Configuration for the Winston logger
+*/
+
+const winston = require("winston");
+const appRoot = require("app-root-path");
+
